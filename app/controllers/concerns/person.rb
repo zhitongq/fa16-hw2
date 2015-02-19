f4eef90dc7ba5c04bbe22b8e76883bc83e9af40b
Charles Xue <charlesx2013@gmail.com>
2015-02-19 03:07:32 -0800

    Finish fa16 hw2

---
+++ app/controllers/concerns/person.rb
@@ -0,0 +1,28 @@
+class Person
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def nickname
+    # YOUR IMPLEMENTATION HERE
+    'nickname'
+  end
+
+  def birth_year
+    # YOUR IMPLEMENTATION HERE
+    1990
+  end
+
+  def introduction
+    # YOUR IMPLEMENTATION HERE
+    'introduce'
+  end
+
+  def fib_number
+    # YOUR IMPLMENTATION HERE
+    0
+  end
+end
---
+++ app/controllers/concerns/stringify.rb
@@ -0,0 +1,11 @@
+class Stringify
+  attr_accessor :name, :adjective
+
+  def initialize(name, adjective)
+    @name = name
+    @adjective = adjective
+  end
+
+  def what_am_i
+  end
+end
